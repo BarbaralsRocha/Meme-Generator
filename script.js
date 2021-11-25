284e0b1f01b8251d667059de31feeefe684b6b9d
BarbaraRocha <barbaraluizasr@gmail.com>
2021-11-25 11:51:25 -0300

    requisito obrigatorios

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const memeImage = document.getElementById('meme-image');
 const fire = document.getElementById('fire');
 const water = document.getElementById('water');
 const earth = document.getElementById('earth');
+const meme1 = document.getElementById('meme-1');
+const meme2 = document.getElementById('meme-2');
+const meme3 = document.getElementById('meme-3');
+const meme4 = document.getElementById('meme-4');
 
 
 function textMeme(){
@@ -42,3 +46,35 @@ function changeEarth(){
     })
 }
 changeEarth()
+
+function meme1Img(){
+    meme1.addEventListener('click', function(){
+        memeImage.src = 'imgs/meme1.png'
+
+    })
+}
+meme1Img()
+
+function meme2Img(){
+    meme2.addEventListener('click', function(){
+        memeImage.src = 'imgs/meme2.png'
+
+    })
+}
+meme2Img()
+
+function meme3Img(){
+    meme3.addEventListener('click', function(){
+        memeImage.src = 'imgs/meme3.png'
+
+    })
+}
+meme3Img()
+
+function meme4Img(){
+    meme4.addEventListener('click', function(){
+        memeImage.src = 'imgs/meme4.png'
+
+    })
+}
+meme4Img()
